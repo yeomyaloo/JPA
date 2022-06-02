9d355226d4fd81930d7d5c810e0faae6842e7933
hwiju yeom <yeom456@gmail.com>
2022-06-02 21:12:56 +0900

    6/2

--- jpashop/src/main/java/jpabook/jpashop/domain/member/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/member/Member.java
@@ -1,28 +1,40 @@
 package jpabook.jpashop.domain.member;
 
-
+import javax.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "MEMBER")
+@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"NAME", "AGE"})})
 public class Member {
 
     @Id
     @Column(name = "ID")
     private String id;
 
-    @Column(name="NAME")
+    @Column(name="NAME", nullable = false, length = 10)
     private String username;
 
     //매핑 보가 없는 필드
     private Integer age;
 
+    // == 추가 ==
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 }
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/member/RoleType.java
@@ -0,0 +1,5 @@
+package jpabook.jpashop.domain.member;
+
+public enum RoleType {
+    ADMIN, USER
+}
